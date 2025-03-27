e99c24e63209867a31bb4e0d728deba224e7327a
DevonGriffith <120066034+DevDarGriff@users.noreply.github.com>
2025-03-26 23:03:37 -0500

    Update ProblemSolutions.java

--- ProblemSolutions.java
+++ ProblemSolutions.java
@@ -1,6 +1,6 @@
 /******************************************************************
  *
- *   YOUR NAME / SECTION NUMBER
+ *   Devon Griffith / COMP 272
  *
  *   This java file contains the problem solutions of isSubSet, findKthLargest,
  *   and sort2Arrays methods. You should utilize the Java Collection Framework for
@@ -8,75 +8,124 @@
  *
  ********************************************************************/
 
-import java.util.*;
+ import java.util.*;
 
-class ProblemSolutions {
+ class ProblemSolutions {
+ 
+     /**
+      * Method: isSubset()
+      *
+      * Given two arrays of integers, A and B, return whether
+      * array B is a subset if array A. Example:
+      *      Input: [1,50,55,80,90], [55,90]
+      *      Output: true
+      *      Input: [1,50,55,80,90], [55,90, 99]
+      *      Output: false
+      *
+      * The solution time complexity must NOT be worse than O(n).
+      * For the solution, use a Hash Table.
+      *
+      * @param list1 - Input array A
+      * @param list2 - input array B
+      * @return      - returns boolean value B is a subset of A.
+      */
+ 
+     public boolean isSubset(int list1[], int list2[]) {
+ 
+         // ADD YOU CODE HERE -- DON'T FORGET TO ADD YOR NAME AT TOP OF FILE
 
-    /**
-     * Method: isSubset()
-     *
-     * Given two arrays of integers, A and B, return whether
-     * array B is a subset if array A. Example:
-     *      Input: [1,50,55,80,90], [55,90]
-     *      Output: true
-     *      Input: [1,50,55,80,90], [55,90, 99]
-     *      Output: false
-     *
-     * The solution time complexity must NOT be worse than O(n).
-     * For the solution, use a Hash Table.
-     *
-     * @param list1 - Input array A
-     * @param list2 - input array B
-     * @return      - returns boolean value B is a subset of A.
-     */
+         HashSet hash = new HashSet<>();
 
-    public boolean isSubset(int list1[], int list2[]) {
+         for (int num : list1) {
+            hash.add(num);
+         }
 
-        // ADD YOU CODE HERE -- DON'T FORGET TO ADD YOR NAME AT TOP OF FILE
+         for (int num : list2) {
+            if (!hash.contains(num)) {
+                return false;
+            }
+         }
+ 
+         return true;
+     }
+ 
+ 
+     /**
+      * Method: findKthLargest
+      *
+      * Given an Array A and integer K, return the k-th maximum element in the array.
+      * Example:
+      *      Input: [1,7,3,10,34,5,8], 4
+      *      Output: 7
+      *
+      * @param array - Array of integers
+      * @param k     - the kth maximum element
+      * @return      - the value in the array which is the kth maximum value
+      */
+ 
+     public int findKthLargest(int[] array, int k) {
+ 
+         // ADD YOUR CODE HERE
 
-        return false;
-    }
+         Queue<Integer> q = new PriorityQueue<>(k);
 
+         for (int i = 0; i < k; i++) {          //add each array element to pq
+            q.add(array[i]);
+         }
 
-    /**
-     * Method: findKthLargest
-     *
-     * Given an Array A and integer K, return the k-th maximum element in the array.
-     * Example:
-     *      Input: [1,7,3,10,34,5,8], 4
-     *      Output: 7
-     *
-     * @param array - Array of integers
-     * @param k     - the kth maximum element
-     * @return      - the value in the array which is the kth maximum value
-     */
+         for (int i = k; i < array.length; i++) {       //finish processing array
+            if (array[i] > q.peek()) {                  //if curr array idx > curr pq root 
+                q.poll();                               //remove smallest element
+                q.add(array[i]);                        //add current element
+            }
+         }
+         return q.peek();                               //return priority queue's root node, which is k'th largest
+        
+     }
+ 
+ 
+     /**
+      * Method: sort2Arrays
+      *
+      * Given two arrays A and B with n and m integers respectively, return
+      * a single array of all the elements in A and B in sorted order. Example:
+      *      Input: [4,1,5], [3,2]
+      *      Output: 1 2 3 4 5
+      *
+      * @param array1    - Input array 1
+      * @param array2    - Input array 2
+      * @return          - Sorted array with all elements in A and B.
+      */
+ 
+     public int[] sort2Arrays(int[] array1, int[] array2) {
+ 
+         // ADD YOU CODE HERE
 
-    public int findKthLargest(int[] array, int k) {
+         //if queue is empty return null
 
-        // ADD YOUR CODE HERE
+         int a1Size = array1.length;
+         int a2Size = array2.length;
+         int size = a1Size + a2Size;
 
-        return 0;
-    }
+         Queue<Integer> queue = new PriorityQueue<>(size);
+         
 
+         for (int i = 0; i < a1Size; i++) {
+            queue.add(array1[i]);
+         }
 
-    /**
-     * Method: sort2Arrays
-     *
-     * Given two arrays A and B with n and m integers respectively, return
-     * a single array of all the elements in A and B in sorted order. Example:
-     *      Input: [4,1,5], [3,2]
-     *      Output: 1 2 3 4 5
-     *
-     * @param array1    - Input array 1
-     * @param array2    - Input array 2
-     * @return          - Sorted array with all elements in A and B.
-     */
+         for (int i = 0; i < a2Size; i++) {
+            queue.add(array2[i]);
+         }
+        
+         int[] result = new int[size];
+         int index = 0;
 
-    public int[] sort2Arrays(int[] array1, int[] array2) {
-
-        // ADD YOU CODE HERE
-
-        return null;
-    }
-
-}
+         while (!queue.isEmpty()) {
+            result[index++] = queue.poll();
+         }
+         
+         return result;
+     }
+ 
+ }
